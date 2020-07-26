5c9942fa29040ed16869a4f3970bdb0bc03d7ba2
Lee <insung.lee@sap.com>
2020-07-26 13:10:50 +0900

    Initial commit

---
+++ src/main/java/com/example/demo2/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public @ResponseBody
+    String root_test()throws Exception{
+        return "Hello World";
+    }
+
+}
